3c4a465de706d47fdd8e7c02810abefd6cb9fd06
ahartloper <alexander.hartloper@epfl.ch>
2019-12-04 09:34:35 +0100

    Add writer for Abaqus analyses.
    - Fill out the abaqus_analysis_writer class.
    - Add the function/command line run_mpc_preprocessor.

--- py/abaqus_analysis_writer.py
+++ py/abaqus_analysis_writer.py
@@ -1,6 +1,30 @@
 class AbaAnalysisWriter:
     def __init__(self, global_model):
+        """ Constructor. """
+        self.model = global_model
+        self.ELEMENTS_HEADER = '*elements'
+        self.INTERFACES_HEADER = '*interface'
         pass
 
     def write(self, output_file):
-        pass
+        """ Writes the file to be read by the Abaqus analysis. """
+        with open(output_file, 'w') as f:
+            l = ','.join([self.ELEMENTS_HEADER, str(len(self.model.interface_elements))]) + '\n'
+            f.write(l)
+            for i, elem in enumerate(self.model.interface_elements):
+                l = ','.join([str(elem), str(self.model.elem_directions[i])]) + '\n'
+                f.write(l)
+            for interf in self.model.interfaces:
+                l = ','.join([self.INTERFACES_HEADER, str(len(interf.interface_nodes))]) + '\n'
+                f.write(l)
+                f.write(str(interf.global_id) + '\n')
+                for n in interf.interface_nodes:
+                    n2e_map = interf.node_to_elem_ind_map[n]
+                    if len(n2e_map) == 1:
+                        l = '{0:d},0,0\n'.format(n2e_map[0])
+                    elif len(n2e_map) == 2:
+                        l = '{0:d},{1:d},0\n'.format(n2e_map[0], n2e_map[1])
+                    else:
+                        l = '{0:d},{1:d},{2:d}\n'.format(n2e_map[0], n2e_map[1], n2e_map[2])
+                    f.write(l)
+        return
---
+++ py/run_mpc_preprocessor.py
@@ -0,0 +1,26 @@
+from __future__ import absolute_import, print_function
+import os
+import sys
+from abaqus_inp_reader import InpReader
+from global_mpc_model import GlobalMpcModel
+from abaqus_analysis_writer import AbaAnalysisWriter
+
+
+def run_mpc_preprocessor(input_file):
+    """ Runs the pre-processor for Abaqus input files that contain inelastic best-fit MPCs. """
+    path_split = os.path.split(input_file)
+    output_file = os.path.join(path_split[0], path_split[1][:-4] + '_interf_props.txt')
+
+    model = GlobalMpcModel()
+    reader = InpReader(model)
+    reader.read(input_file)
+    writer = AbaAnalysisWriter(model)
+    writer.write(output_file)
+    return
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        run_mpc_preprocessor(sys.argv[1])
+    else:
+        print('Usage:\n\tpython run_mpc_preprocessor <path_to_input_file.inp>')
